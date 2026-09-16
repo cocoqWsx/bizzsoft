4f1d9e20b941d870b4526ad018f94608fae93cfe
cocoqWsx <100495787+cocoqWsx@users.noreply.github.com>
2026-09-16 14:52:29 -0500

    Enable SSL redirection and secure cookies in settings

--- bizzsoft/settings.py
+++ bizzsoft/settings.py
@@ -87,5 +87,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
